bb13bd769c50fb28acd1038c1a0ea3a304b5b0f7
soutik-d <89070337+soutik-d@users.noreply.github.com>
2021-12-27 18:11:16 +0530

    Add files via upload

--- SyncRequirementFromAzureDevopsWorkItem.js
+++ SyncRequirementFromAzureDevopsWorkItem.js
@@ -10,8 +10,8 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
 <b>Type:</b> ${fields["System.WorkItemType"]}<br>
 <b>Area:</b> ${fields["System.AreaPath"]}<br>
 <b>Iteration:</b> ${fields["System.IterationPath"]}<br>
-<b>Reason:</b> ${fields["System.Reason"]}<br>
-<b>Description:</b> ${fields["System.Description"] || ""}`;
+<b>Description:</b> ${fields["System.Description"]}<br>
+<b>Acceptance Criteria:</b> ${fields["Microsoft.VSTS.Common.AcceptanceCriteria"] || ""}`;
     }
     
     function buildRequirementName(namePrefix, eventData) {
@@ -23,12 +23,54 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
         const fields = getFields(eventData);
         return `${fields["Microsoft.VSTS.Scheduling.StoryPoints"]}`;
     }
+	function buildAzureFeatureId(eventData)
+	{
+		 const fields = getFields(eventData);
+        return `${fields["System.Parent"]}`;
+	}
     function getFields(eventData) {
         // In case of update the fields can be taken from the revision, in case of create from the resource directly
         return eventData.resource.revision ? eventData.resource.revision.fields : eventData.resource.fields;
     }
-
-    const standardHeaders = {
+    async function getQtestAuthToken()
+	{
+		const url=`https://sdtest.qtestnet.com/oauth/token`;
+		const requestBody=`grant_type:password&username:jovanispy@anuefa.com&password:abc12345`;
+		let failed=false;
+		let authDet=undefined;
+		try{
+			const response = await doqTestPostAuthRequest(url, "POST", requestBody);
+			console.log("Auth API Response :",JSON.stringify(response));
+            if (!response || response.length === 0) {
+                console.log("[Info] Response Unavailable.");
+				failed = 1;
+            } else {
+                if (response.length === 1) {
+                    authDet = response;
+                } else {
+                    failed = 2;
+                    console.log("[Warn] Multiple Authorization found.");
+                }
+            }
+        } catch (error) {
+            console.log("[Error] Failed to get Authorization", error);
+            failed = error;
+        }
+        return { failed: failed, authDet: authDet };
+	}
+	
+	//const qTestTokenDetails=await getQtestAuthToken();
+	//console.log("Qtest Token Details :",JSON.stringify(qTestTokenDetails));
+	//let qTestToken=undefined;
+	//if (qTestTokenDetails.failed)
+	//{
+	//	qTestToken=`${qTestTokenDetails.failed}`;
+	//}
+	//else
+	//{
+	//	qTestToken=qTestTokenDetails.access_token;
+    //}
+	const standardHeaders = {
         "Content-Type": "application/json",
         Authorization: `bearer ${constants.QTEST_TOKEN}`,
     };
@@ -37,13 +79,29 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
         UPDATED: "workitem.updated",
         DELETED: "workitem.deleted",
     };
-
+	
+	let returnedModuleDetails=undefined;
+	
     let workItemId = undefined;
     let requirementToUpdate = undefined;
+	const reqAzureFeatureId=buildAzureFeatureId(event);
+	let prefixAzFeature=undefined;
+	let returnedModuleId=undefined;
     switch (event.eventType) {
         case eventType.CREATED: {
             workItemId = event.resource.id;
-            console.log(`[Info] Create workitem event received for 'WI${workItemId}'`);
+ 
+			prefixAzFeature=`WI${reqAzureFeatureId}`;
+			const modURL=`https://${constants.ManagerURL}/api/v3/projects/${constants.ProjectID}/modules?search=${prefixAzFeature}`;
+			const moddet1=await searchQtestModuleByAzureFeatureId(modURL);
+			const moddetString=JSON.stringify(moddet1)
+			//console.log("Returned Module String: ", moddetString);
+			const splString=moddetString.split(",");
+			const modIdSplit=splString[2].split(":");
+			//console.log("String After split",modIdSplit[1]);
+			returnedModuleId=modIdSplit[1];
+			console.log("Returned Module ID",returnedModuleId);
+			
             break;
         }
         case eventType.UPDATED: {
@@ -79,18 +137,18 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
             console.log(`[Error] Unknown workitem event type '${event.eventType}' for 'WI${workitemId}'`);
             return;
     }
-
+	
     // Prepare data to create/update requirement
     const namePrefix = getNamePrefix(workItemId);
     const requirementDescription = buildRequirementDescription(event);
     const requirementName = buildRequirementName(namePrefix, event);
 	const reqAzureStoryId= getAzStoryId(workItemId);
 	const reqAzureStoryPoints=buildRequirementStoryPoints(event);
-
+	
     if (requirementToUpdate) {
         await updateRequirement(requirementToUpdate, requirementName, requirementDescription, reqAzureStoryPoints);
     } else {
-        createRequirement(requirementName, requirementDescription, reqAzureStoryId, reqAzureStoryPoints);
+        createRequirement(requirementName, requirementDescription, reqAzureStoryId,reqAzureStoryPoints, returnedModuleId);
     }
 
     function getNamePrefix(workItemId) {
@@ -131,13 +189,20 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
             console.log("[Error] Failed to get requirement by work item id.", error);
             failed = true;
         }
-
         return { failed: failed, requirement: requirement };
     }
-
+	
+	async function searchQtestModuleByAzureFeatureId(prefix) {
+        const url =prefix; //`https://${constants.ManagerURL}/api/v3/projects/${constants.ProjectID}/modules?search=${prefix}`;
+        const response = await doRequest(url, "GET", null);
+        console.log(response);
+        return response;      
+    }
+	
     async function updateRequirement(requirementToUpdate, name, description, storyPoints) {
         const requirementId = requirementToUpdate.id;
         const url = `https://${constants.ManagerURL}/api/v3/projects/${constants.ProjectID}/requirements/${requirementId}`;
+		
         const requestBody = {
             properties: [
                 {
@@ -162,35 +227,12 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
         }
     }
 
-    async function updateRequirementCTEdetails(createdRequirementID, createdRequirementPid) {
-        //const requirementId = requirementToUpdate.id;
-        const url = `https://${constants.ManagerURL}/api/v3/projects/${constants.ProjectID}/requirements/${createdRequirementID}`;
-        const requestBody = {
-            name: name,
-            properties: [
-                {
-                    field_id: constants.CTEDetailsFieldId,
-                    field_value: `https://app.powerbi.com/groups/me/reports/2e3c07cf-8aea-4e0c-ac2a-40661d0771df/ReportSection2e319d649be4649a65b1?chromeless=1&filter=Requirements%2FId%20eq%20%27RQ-18%27`,
-                },
-				
-            ],
-        };
-
-        console.log(`[Info] Updating requirement '${createdRequirementID}'.`);
-
-        try {
-            await put(url, requestBody);
-            console.log(`[Info] Requirement '${createdRequirementID}' updated.`);
-        } catch (error) {
-            console.log(`[Error] Failed to update requirement '${createdRequirementID}'.`, error);
-        }
-    }
-
-    async function createRequirement(name, description, azureStoryId, azureStoryPoints) {
+    async function createRequirement(name, description, azureStoryId, azureStoryPoints, patModule) {
         const url = `https://${constants.ManagerURL}/api/v3/projects/${constants.ProjectID}/requirements`;
-        const requestBody = {
+		//const desc=`${qTestToken}`;
+		const requestBody = {
             name: name,
-            parent_id: constants.RequirementParentID,
+            parent_id: patModule, 
             properties: [
                 {
                     field_id: constants.RequirementDescriptionFieldID,
@@ -215,9 +257,6 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
 
         try {
             const response= await post(url, requestBody);
-            const createdReqId=response.pid;
-            const createdReqPid=response.pid;
-            await updateRequirementCTEdetails(createdReqId,createdReqPid);
             console.log(`[Info] Requirement created.`);
         } catch (error) {
             console.log(`[Error] Failed to create requirement`, error);
@@ -245,6 +284,10 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
     function put(url, requestBody) {
         return doRequest(url, "PUT", requestBody);
     }
+	function postAuth(url, requestBody)
+	{
+		return doqTestPostAuthRequest(url, "POST", requestBody);
+	}
 
     async function doRequest(url, method, requestBody) {
         const opts = {
@@ -267,6 +310,61 @@ exports.handler = async function ({ event, constants, triggers }, context, callb
                     return;
                 }
 
+                resolve(body);
+            });
+        });
+    }
+	async function doqTestGetRequest(url, method, requestBody) {
+        const qTestHeaders = {
+            "Content-Type": "application/json",
+            Authorization: `bearer ${constants.QTEST_TOKEN}`,
+        };
+        const opts = {
+            url: url,
+            json: true,
+            headers: qTestHeaders,
+            body: requestBody,
+            method: method,
+        };
+
+        return new Promise((resolve, reject) => {
+            request(opts, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+
+                if (response.statusCode < 200 || response.statusCode >= 300) {
+                    reject(`HTTP ${response.statusCode}`);
+                    return;
+                }
+
+                resolve(body);
+            });
+        });
+    }
+	async function doqTestPostAuthRequest(url, method, requestBody) {
+        const qTestAuthHeaders = {  
+			 "Content-Type": "application/x-www-form-urlencoded",
+			  Authorization: `basic ${constants.QTEST_bAuth_TOKEN}`,
+        };
+        const opts = {
+            url: url,
+			json: true,
+            headers: qTestAuthHeaders,
+            body: requestBody,
+            method: method,
+        };
+        return new Promise((resolve, reject) => {
+            request(opts, function (error, response, body) {
+                if (error) {
+                    reject(error);
+                    return;
+                }
+                if (response.statusCode < 200 || response.statusCode >= 300) {
+                    reject(`HTTP ${response.statusCode}`);
+                    return;
+                }
                 resolve(body);
             });
         });
